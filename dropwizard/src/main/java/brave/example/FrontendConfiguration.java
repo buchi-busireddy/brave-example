a57ff1fe7800a62ae7afd5323373a09736e48a91
Buchi Reddy Busi Reddy <buchi@traceable.ai>
2020-11-05 12:56:26 -0800

    Minor cleanup.

--- dropwizard/src/main/java/brave/example/FrontendConfiguration.java
+++ dropwizard/src/main/java/brave/example/FrontendConfiguration.java
@@ -2,8 +2,38 @@ package brave.example;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.client.JerseyClientConfiguration;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class FrontendConfiguration extends Configuration {
+  static class BackendConfiguration extends Configuration {
+    @NotNull String endpoint = "http://127.0.0.1:9000/api";
+
+    @Valid
+    @NotNull
+    private JerseyClientConfiguration jerseyClient = new JerseyClientConfiguration();
+
+    @JsonProperty public String getEndpoint() {
+      return endpoint;
+    }
+
+    @JsonProperty public void setEndpoint(String endpoint) {
+      this.endpoint = endpoint;
+    }
+
+    @JsonProperty("jerseyClient")
+    public JerseyClientConfiguration getJerseyClientConfiguration() {
+      return jerseyClient;
+    }
+
+    @JsonProperty("jerseyClient")
+    public void setJerseyClientConfiguration(JerseyClientConfiguration jerseyClient) {
+      this.jerseyClient = jerseyClient;
+    }
+  }
+
   final BackendConfiguration backend = new BackendConfiguration();
 
   @JsonProperty public BackendConfiguration getBackend() {
